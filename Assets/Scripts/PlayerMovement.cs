83dabb47e20f358e5b82a4beae0f2d8a85393d6c
RizarOne <120001673+RizarOne@users.noreply.github.com>
2023-09-07 10:29:19 +0300

    Better Jump
    Player now has better jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
 
     public Animator animator;
     public Rigidbody2D rb2D;
@@ -57,6 +59,14 @@ public class PlayerMovement : MonoBehaviour
         {
             rb2D.velocity = new Vector2(0, jumpForce);
             //animator.SetTrigger("Jump"); // Kun hypylle animaatio.
+        }
+        if (rb2D.velocity.y < 0)
+        {
+            rb2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb2D.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
 
         hearts.fillAmount = GameManager.manager.health/ GameManager.manager.maxHealth;
