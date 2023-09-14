aee3fccf9a9d68ce652b9c11566456fe17c622e9
RizarOne <120001673+RizarOne@users.noreply.github.com>
2023-09-14 18:55:14 +0300

    Enemy and map scene
    Added a-b pathfinding script to potato enemy. Added background to map scene.

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb2D;
+    private Animator anim;
+    private Transform currentPoint;
+    public float speed;
+
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        anim.SetBool("isMoving", true);
+    }
+
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform)
+        {
+            rb2D.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb2D.velocity = new Vector2(-speed, 0);
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            flip();
+            currentPoint = pointA.transform;
+        }
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            flip();
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 1f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 1f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    }
+}
